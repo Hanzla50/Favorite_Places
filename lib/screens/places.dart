a543932bc063ef023aa8da3134ba3db7e4b3a01b
Hanzla50 <hanzlajaved50281@gmail.com>
2024-07-31 18:53:28 +0500

    Completed(Image picker, localy stored data in sql database)

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,12 +1,29 @@
+import 'package:favorite_places/provider/user_places.dart';
 import 'package:favorite_places/screens/add_place.dart';
 import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _PlacesScreen();
+  }
+}
+
+  class _PlacesScreen extends ConsumerState<PlacesScreen> {
+    late Future<void> _placesFuture;
+
+    @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+  @override
   Widget build(BuildContext context) {
+    final userPlaces = ref.watch(userPlacesProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
@@ -22,8 +39,12 @@ class PlacesScreen extends StatelessWidget {
               icon: const Icon(Icons.add))
         ],
       ),
-      body: PlacesList(
-        places: [],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(future: _placesFuture, builder: (context, snapshot) => snapshot.connectionState == ConnectionState.waiting ? const Center(child: CircularProgressIndicator(),) : PlacesList(
+          places: userPlaces,
+        ),)
+        
       ),
     );
   }
--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/model/place.dart';
+import 'package:favorite_places/screens/place_detail.dart';
 import 'package:flutter/material.dart';
 
 class PlacesList extends StatelessWidget {
@@ -13,18 +14,25 @@ class PlacesList extends StatelessWidget {
         child: Text('NO places added yet!',
             style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                   color: Theme.of(context).colorScheme.onBackground,
-                )),
+                ),),
       );
     }
     return ListView.builder(
       itemCount: places.length,
       itemBuilder: (ctx, index) => ListTile(
+        leading: CircleAvatar(
+          radius:   26,
+          backgroundImage: FileImage(places[index].image) ,
+        ),
         title: Text(
           places[index].title,
           style: Theme.of(context).textTheme.titleMedium!.copyWith(
                 color: Theme.of(context).colorScheme.onBackground,
               ),
         ),
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => PlaceDetailScreen(place: places[index])));
+        },
       ),
     );
   }
